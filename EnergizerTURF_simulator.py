134857841069656795f7744e0aa3fdb035fabad4
jelisavetaM <107268735+jelisavetaM@users.noreply.github.com>
2022-07-29 02:49:40 +0200

    Update EnergizerTURF_simulator.py

--- EnergizerTURF_simulator.py
+++ EnergizerTURF_simulator.py
@@ -67,7 +67,7 @@ with st.sidebar:
             originalTURF = originalTURF.query('AGE == "' + ageFilter + '"')
     st.markdown('#')
     st.markdown('#')
-    with st.expander('Instructions'):
+    with st.expander('How to use TURF simulator?'):
         st.markdown("To be added...",unsafe_allow_html = True)
         
 allColumns = list(originalTURF.columns)
