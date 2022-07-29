aed724fb682e9a06d29a78cdce567e3349e8cfc5
jelisavetaM <107268735+jelisavetaM@users.noreply.github.com>
2022-07-29 02:42:22 +0200

    Update EnergizerTURF_simulator.py

--- EnergizerTURF_simulator.py
+++ EnergizerTURF_simulator.py
@@ -2,20 +2,6 @@
 import pandas as pd
 import streamlit as st
 import altair as alt
-import streamlit_authenticator as stauth
-
-with open('config.yaml') as file:
-    config = yaml.load(file, Loader=SafeLoader)
-
-authenticator = Authenticate(
-    config['credentials'],
-    config['cookie']['name'],
-    config['cookie']['key'],
-    config['cookie']['expiry_days'],
-    config['preauthorized']
-)
-
-name, authentication_status, username = authenticator.login('Login', 'main')
 
 @st.cache 
 def make_id_sets(dataframe):
