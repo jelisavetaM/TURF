399f3422774aab6d57441a4b354ae8a9c155391b
jelisavetaM <107268735+jelisavetaM@users.noreply.github.com>
2022-07-29 13:39:30 +0200

    Update EnergizerTURF_simulator.py

--- EnergizerTURF_simulator.py
+++ EnergizerTURF_simulator.py
@@ -129,7 +129,8 @@ if calc:
     finalTarget.append('USERID')
     originalTURF = originalTURF[[col for col in finalTarget]]
     sets = make_id_sets(originalTURF)
-    originalTURF = originalTURF.drop('None of the above', axis=1)
+    if 'None of the above' in originalTURF.columns:
+        originalTURF = originalTURF.drop('None of the above', axis=1)
     order, percentages = calculate_order_percentages(sets,125,originalTURF,originalTURF.columns.get_loc(originalTURF.drop(['USERID','CHANNEL','GENDER','AGE'], axis=1).sum().idxmax()))
 
     res = {order[i]: percentages[i] for i in range(len(order))}
