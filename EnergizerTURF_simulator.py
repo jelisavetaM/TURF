e54ba9daf1c800a78e5f7545b425ac22a114014e
jelisavetaM <107268735+jelisavetaM@users.noreply.github.com>
2022-07-31 18:32:06 +0200

    Update EnergizerTURF_simulator.py

--- EnergizerTURF_simulator.py
+++ EnergizerTURF_simulator.py
@@ -46,7 +46,7 @@ originalTURF = pd.read_csv('Merged.csv')
 # upload data
 originalTURF_temp = st.empty()
 originalTURF_temp = st.file_uploader("Upload a TURF CSV file", accept_multiple_files=False)
-if originalTURF is not None:
+if originalTURF_temp is not None:
     originalTURF = originalTURF_temp
     originalTURF_temp.empty()
 
