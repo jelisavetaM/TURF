18fc94846806e353b02b79b2ce2c8be52630a2f5
jelisavetaM <107268735+jelisavetaM@users.noreply.github.com>
2022-07-31 17:01:33 +0200

    Update EnergizerTURF_simulator.py

--- EnergizerTURF_simulator.py
+++ EnergizerTURF_simulator.py
@@ -151,7 +151,7 @@ if calc:
     resToDF = resToDF[['SKU','Reach %','Increment']]
     # st.write(resToDF.astype(str))
     resToDF.drop(resToDF[resToDF['SKU'] == "USERID"].index, inplace = True)
-    resToDFSize = st.number_input('Define reesult size (the default is maximum number of SKUs that have incremental value to reach).', min_value = 2, max_value = resToDFSize.shape[1], value = resToDFSize.shape[1], step = 1)
+    resToDFSize = st.number_input('Define reesult size (the default is maximum number of SKUs that have incremental value to reach).', min_value = 2, max_value = resToDF.shape[1], value = resToDF.shape[1], step = 1)
     resToDF = resToDF.iloc[0:resToDFSize]
     st.table(resToDF)
 
