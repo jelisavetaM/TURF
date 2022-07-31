77b1f047f70b1b696a19d8be9a3a8c12e9ec49a6
jelisavetaM <107268735+jelisavetaM@users.noreply.github.com>
2022-07-31 17:45:26 +0200

    Update EnergizerTURF_simulator.py

--- EnergizerTURF_simulator.py
+++ EnergizerTURF_simulator.py
@@ -39,9 +39,7 @@ originalTURF = pd.read_csv('Merged.csv')
 # Add filters for respondents
 with st.sidebar:
 
-    st.markdown("<div style='color:red; font-size:30px; position:absolute; top:-8vh;'>EyeSee TURF simulator<br><p>Air Freshener Product Optimization project</p></div>", unsafe_allow_html=True)
-    st.caption("Air Freshener Product Optimization project")
-   
+    st.markdown("<div style='color:red; font-size:30px; position:absolute; top:-8vh;'>EyeSee TURF simulator<br><p style='color:gray'>Air Freshener Product Optimization project</p></div>", unsafe_allow_html=True)
     st.markdown("#")
     st.markdown("#")
     st.caption("<p style='color: white, font-family: Source Sans Pro, sans-serif'>Select channel:</p>", unsafe_allow_html=True)
