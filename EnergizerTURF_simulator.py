c5d310812b868bd3f6eeae8feb2acad3fbf41ab4
jelisavetaM <107268735+jelisavetaM@users.noreply.github.com>
2022-08-01 17:35:45 +0200

    Update EnergizerTURF_simulator.py

--- EnergizerTURF_simulator.py
+++ EnergizerTURF_simulator.py
@@ -97,6 +97,7 @@ with st.sidebar:
     st.markdown("<div style='color:#ff4b4b; font-size:30px; position:absolute; top: -8vh;'>EyeSee TURF simulator<br><p style='color:white'>Air Freshener Product Optimization project</p></div>", unsafe_allow_html=True)
 with st.container():
     originalTURF_temp = login()
+    st.stop()
     with st.sidebar:
         st.markdown("#")
         st.markdown("#")
